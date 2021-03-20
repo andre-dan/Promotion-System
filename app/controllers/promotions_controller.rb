db8ee2ec312faf8e0b67008f77019a89455617a4
Andre Torres <andrerndc@gmail.com>
2021-03-20 01:21:35 -0300

    Cadastrar promoçao validando os campos

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -14,7 +14,7 @@ class PromotionsController < ApplicationController
 
   def create
     @promotion = Promotion.new(params.require(:promotion).permit(:name, :description, :code, :discount_rate,
-                                                                 :coupon_quantity, :expiration_date))
+                                                                 :coupon_quantity, :expiration_date))                                                                
     if @promotion.save
       redirect_to @promotion
     else
