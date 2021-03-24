8d56d370473ca036290ede6afa3d312dc96341f9
Andre Torres <andrerndc@gmail.com>
2021-03-24 14:03:15 -0300

    Desabilitando Cupons

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,8 @@
+class CouponsController < ApplicationController
+
+  def disable
+    @coupon = Coupon.find(params[:id])
+    @coupon.disabled!
+    redirect_to @coupon.promotion, notice: t('.success', code: @coupon.code)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :promotions do
     post 'generate_coupons', on: :member
   end
-
+  resources :coupons, only: [] do
+    post 'disable', on: :member
+  end
   resources :product_categories
 end
